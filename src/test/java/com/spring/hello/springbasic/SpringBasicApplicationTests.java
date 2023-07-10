db3f37ef6bced18947e619175b8650c981f28b8a
tmfrl1570 <tmfrl1570@naver.com>
2023-07-11 00:26:52 +0900

    project-create

---
+++ src/test/java/com/spring/hello/springbasic/SpringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.hello.springbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
