d9e8942ae8424d8e2f98156f245aaf814bddc9a3
tmfrl1570 <tmfrl1570@naver.com>
2023-07-11 09:58:23 +0900

    repository

--- src/main/java/com/spring/hello/springbasic/controller/HelloController.java
+++ src/main/java/com/spring/hello/springbasic/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.spring.hello.springbasic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,4 +13,42 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+
+
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody   // http 헤더와 바디 중에 바디에 내용을 직접 넣어줌
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
